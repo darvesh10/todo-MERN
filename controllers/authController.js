3cfcbe380b5bd9f5e63f88cd5b7c36a6d456548b
darvesh soni <darveshsoni1@gmail.com>
2025-08-22 20:13:32 +0530

    feat: implement user registration and login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,2 +1,55 @@
 const User = require('../models/User');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const genrateToken = require('../utils/generateToken');
+
+const registerUser = async (req,res,next) => {
+     const {name, email, password} = req.body;
+     try{
+        if(!name || !email || !password) {
+            return res.status(400).json({message: "please fill all fields"})
+        }
+     
+     const userExists  = await User.findOne({email});
+     if(userExists) {
+       return res.status(400).json({message: "user already exists"});
+     }
+     
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: genrateToken(user._id),
+    })
+   } catch(error){
+      next(error);
+   }
+};
+
+
+const loginUser = async (req,res,next) => {
+   const {email,password} = req.body;
+   try{
+      const user = await User.findOne({email});
+      if(user && (await bcrypt.compare(password,user.password))){
+         res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: genrateToken(user._id)
+         });
+      } else{
+         res.status(401).json({message: "invalid email or password"});
+      }
+
+   } catch(error){
+      next(error)
+   }
+};
+
+module.exports = {registerUser, loginUser}
--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const express = require('express');
 const app = express();
 const {notFound,errorHandler} = require('./middleware/errorHandling');
 const {connectDB} = require('./config/db')
+const authRoutes = require('./routes/authRoutes');
+const cors = require("cors");
 
 
 dotenv.config();
 connectDB();
 
+
+app.use(express.json());
+app.use(cors());
+
 app.get('/',(req,res)=>{
     res.send("this is a page");
 })
+app.use("/todo",authRoutes)
 
 
 app.use(notFound);
